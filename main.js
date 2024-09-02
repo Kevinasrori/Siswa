fcb72abe3494bb116eaa3a2274a63460e87af8b0
kevin <Kp8007926@gmail.com>
2024-09-02 10:08:16 +0700

    Menambah fungsi ambil daftar siswa

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
 import {
-  getFirestore, 
-  collection, 
+  getFirestore,
+  collection,
   doc,
-  getDocs, 
-  getDoc, 
-  addDoc, 
-  deleteDoc, 
-  updateDoc, 
-  query, 
+  getDocs,
+  getDoc,
+  addDoc,
+  deleteDoc,
+  updateDoc,
+  query,
   orderBy
 } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
-  
+
 const firebaseConfig = {
   apiKey: "AIzaSyBerOBhNO7D2T11bd1s4_VycsvQArOXwcY",
-  authDomain: "insan-cermelang-fb8a6.firebaseapp.com",
+  authDomain: "insan-cerme.lang-fb8a6.firebaseapp.com",
   projectId: "insan-cermelang-fb8a6",
   storageBucket: "insan-cermelang-fb8a6.appspot.com",
   messagingSenderId: "80632964152",
@@ -25,3 +25,21 @@ const firebaseConfig = {
 // inisialisasi firebase
 const aplikasi = initializeApp(firebaseConfig)
 const basidata = getFirestore(aplikasi)
+
+export async function ambilDaftarsiswaw()
+const refdokumen = collection(basidata,"siswa");
+const kueri = query(refdokumen, orderBy("nama"));
+const cuplikankueri = await getDocs(kueri);
+
+let hasilkueri = [];
+cuplikankueri.forEach((dokumen) => {
+  hasilkueri.push({
+    id: dokumen.id, 
+    nama: dokumen.data().nama
+    alamat: dokumen.data().alamat
+  })
+ })
+ 
+ return hasilkueri;
+
+}
